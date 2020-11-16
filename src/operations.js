80a94db6d63673b7cb2215776087043b46404d5d
Ollie Williams <ollie.williams@ibm.com>
2020-11-16 19:32:12 +0000

    chore: moved operations into a dedicated file, simplifying the calculator

--- src/operations.js
+++ src/operations.js
@@ -1,11 +1,17 @@
+const { ADD, SUBTRACT, MULTIPLY, DIVIDE } = require('./constants');
+
 const add = (a) => (b) => a + b;
 const subtract = (a) => (b) => a - b;
 const multiply = (a) => (b) => a * b;
 const divide = (a) => (b) => a / b;
 
+const OPERATIONS = new Map([
+  [ADD, add],
+  [SUBTRACT, subtract],
+  [MULTIPLY, multiply],
+  [DIVIDE, divide],
+]);
+
 module.exports = {
-  add,
-  subtract,
-  multiply,
-  divide,
+  OPERATIONS,
 };
